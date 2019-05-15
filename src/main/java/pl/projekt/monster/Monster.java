83bce15f1eff561c462abccff3b837505265fee1
el_glowa <kabanos11988@gmail.com>
2019-05-15 14:15:27 +0200

    2nd commit

--- src/main/java/pl/projekt/monster/Monster.java
+++ src/main/java/pl/projekt/monster/Monster.java
@@ -1,4 +1,26 @@
 package pl.projekt.monster;
 
-public class Monster {
+public abstract class Monster {
+    private double healthPoints;
+    private double defencePoints;
+    private double attacPoints;
+
+    public void setAttacPoints(double attacPoints) {
+        this.attacPoints = attacPoints;
+    }
+    public double getAttacPoints() {
+        return attacPoints;
+    }
+    public void setDefencePoints(double defencePoints) {
+        this.defencePoints = defencePoints;
+    }
+    public double getDefencePoints() {
+        return defencePoints;
+    }
+    public void setHealthPoints(double healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+    public double getHealthPoints() {
+        return healthPoints;
+    }
 }
