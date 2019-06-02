f9b4095a653d8ac548f180979a735635d7c6fcfd
TheSausages <kziejlo@gmail.com>
2019-06-02 18:44:21 +0200

    Test v1

--- src/main/java/pl/projekt/game/mob/Orc.java
+++ src/main/java/pl/projekt/game/mob/Orc.java
@@ -29,13 +29,22 @@ public class Orc extends AbstractMonster{
     }
 
     @Override
-    public void addArmour(double armr) { super.addArmour(armr); }
+    public void addArmour(double armr) { this.defencePoints += armr; }
 
     @Override
-    public void addHP(double HP) { super.addHP(HP); }
+    public void addHP(double HP) { this.healthPoints += HP; }
 
     @Override
-    public void addAttack(double attack) { super.addAttack(attack); }
+    public void addAttack(double attack) { this.attacPoints +=attack; }
+
+    @Override
+    public double getAttack() { return attacPoints; }
+
+    @Override
+    public double getHealth() { return healthPoints; }
+
+    @Override
+    public double getDefence() { return defencePoints; }
 
     /**
      * metoda zwiększa ilość posiadanego przez moba kamienia o 1
